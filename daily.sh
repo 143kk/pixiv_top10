b0bc235d7f6c2a14a7cbe8aa1c3308a4826be4e3
ar <ar@ardeMacBook-Pro.local>
2018-02-26 22:27:32 +0800

    Add daily.sh

--- daily.sh
+++ daily.sh
@@ -1,8 +1,9 @@
 cd /home/pixiv_top10
-if $1
-    DATE = $1
+if [ "$1" ]
+then
+    DATE="$1"
 else
     DATE=$(date -d last-day +%Y%m%d)
 fi
 python3 pixivWithDate.py $DATE
-qshell quaload 10 /root/qshell.conf
+qshell qupload 10 /root/qshell.conf
